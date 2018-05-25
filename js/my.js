e87dfd862c5221a69cf478f570a52fc2970df89e
khalvin <you@example.com>
2018-05-25 13:59:08 +0800

    first commit donw upto second section

---
+++ js/my.js
@@ -0,0 +1,15 @@
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        typeSpeed: 50,
+        backSpeed: 0,
+        backDelay: 1000,
+        startDelay: 2000,
+        fadeOut:true,
+        loop: true,
+      });
+});
+
