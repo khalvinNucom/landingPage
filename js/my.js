cb0bc037681a754e93ba2604bd40bdefc70e1c53
khalvin <you@example.com>
2018-05-28 10:58:49 +0800

    ProgressBar finished

--- js/my.js
+++ js/my.js
@@ -1,6 +1,6 @@
 
-
 $( document ).ready(function() {
+    //typed-----------------------------------------------------------
     console.log( "ready!" );
     var typed = new Typed('#typed', {
         stringsElement: '#typed-strings',
@@ -11,5 +11,29 @@ $( document ).ready(function() {
         fadeOut:true,
         loop: true,
       });
+    //typed-----------------------------------------------------------
+    move('.skills__right1', 89);
+    move('.skills__right2', 80);
+    move('.skills__right3', 75);
+    move('.skills__right4', 75);
+    move('.skills__right5', 80);
+
+    //progressBar animation ---------------------------------------------------
+    function move(className, perc) {
+        var elem = document.querySelector(className+" .bar");   
+        console.log(className);
+        var width = 10;
+        var id = setInterval(frame, 10);
+        function frame() {
+          if (width >= perc) {
+            clearInterval(id);
+          } else {
+            width++; 
+            elem.style.width = width + '%'; 
+            elem.innerHTML = width * 1  + '%';
+          }
+        }
+      }
+
 });
 
